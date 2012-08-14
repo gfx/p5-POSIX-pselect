964952b0bba0b11191af764da955e30d62f4a90d
Fuji, Goro <g.psy.va@gmail.com>
2012-08-15 08:44:00 +0900

    Checking in changes prior to tagging of version 0.03.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 601048a..56d4f49 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,8 @@
     Revision history for Perl extension POSIX::pselect
    
    +0.03 2012-08-15 08:42:25
    +    - Build on 5.15.3 (Leont)
    +
     0.02 2011-06-24 11:36:09
         - Fix dependencies
         - Enable tests all the time

--- lib/POSIX/pselect.pm
+++ lib/POSIX/pselect.pm
@@ -3,7 +3,7 @@ use 5.008_001;
 use strict;
 use warnings;
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 use Exporter ();
 our @ISA    = qw(Exporter);
@@ -21,7 +21,7 @@ POSIX::pselect - Perl interface to pselect(2)
 
 =head1 VERSION
 
-This document describes POSIX::pselect version 0.02.
+This document describes POSIX::pselect version 0.03.
 
 =for test_synopsis my($rfdset, $wfdset, $efdset, $timeout, $sigset);
 
